b17becc0548d38e3855585e65bfbc262c2a30b01
AkramjonDev <akramjon.ikromov0523@gmail.com>
2026-02-27 15:12:38 +0500

    Golang

---
+++ practice/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("Hello World practice" + " " + "Hello second")
+	fmt.Println("Hello World second")
+}
+
+func main2(){
+	fmt.Println("Hello first" + "Hello second")
+}
