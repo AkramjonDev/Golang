de34044d0f8a8796562b9e7f5bf3681c73beecc1
AkramjonDev <akramjon.ikromov0523@gmail.com>
2026-03-22 23:47:32 +0500

    continue

--- 1/Lec1/main.go
+++ 1/Lec1/main.go
@@ -1,4 +1,4 @@
-package main //package name
+package main
 
 import "fmt"
 
--- practice/main.go
+++ practice/main.go
@@ -2,12 +2,88 @@ package main
 
 import "fmt"
 
-func main(){
-	fmt.Println("Hello World practice" + " " + "Hello second")
-	fmt.Println("Hello World second")
-	fmt.Print("checking")
-}
+// func main(){
+// 	fmt.Println("Hello World practice" + " " + "Hello second")
+// 	fmt.Println("Hello World second")
+// 	fmt.Print("checking")
+// }
 
-func main2(){
-	fmt.Println("Hello first" + "Hello second")
+// func main2(){
+// 	fmt.Println("Hello first" + "Hello second")
+// }
+
+// func main(){
+// 	var f1 int = 100;
+// 	f1 = 200
+
+// 	fmt.Println(f1)
+// }
+
+// func main(){
+// 	// any value 
+// 	a := fmt.Sprintf("Meni yoshim %v da", 20)
+// 	fmt.Println(a)
+
+// 	// string version
+// 	b := fmt.Sprintf("Meni %s bor", "tajribam")
+// 	fmt.Println(b)
+
+// 	// Integer version
+// 	c := fmt.Sprintf("I have %d years of experience", 2)
+// 	fmt.Println(c)
+
+// 	// Float version
+// 	d := fmt.Sprintf("I have %f experience", 10.1334)
+// 	d2 := fmt.Sprintf("I have %.2f experience", 10.1234)
+
+// 	fmt.Println(d, d2)
+// }
+
+// func main() {
+// 	senderName := "Syl"
+// 	recipient := "Kaladin"
+// 	message := "The Words, Kaladin. You have to speak the Words!"
+
+// 	fmt.Printf("%s to %s, %s\n", senderName, recipient, message)
+// }
+
+
+
+// func main() {
+// 	word1 := "имя"
+// 	word2 := "твое"
+// 	word3 := "мне"
+// 	word4 := "знакомо"
+
+// 	fmt.Println(word4, word3, word2, word1)
+// 	fmt.Println(word3, word1, word4, word2)
+// 	fmt.Println(word2, word4, word1, word3)
+// }
+
+
+// func main(){
+// 	var (
+// 		name string = "akmal"
+// 		age int = 13
+// 	)
+
+// 	fmt.Printf("My name %s, and my age is %d", name, age)
+
+// }
+
+func main(){
+var (
+name string = "Аркадий"
+surname string = "Петров"
+age int = 19
+)
+var (
+	name2 string = "Елена"
+	surname2 string = "Сидорова"
+	age2 int = 19
+)
+
+fmt.Printf("Имя: %s, Фамилия: %s, Возраст: %d, Студент BPS", name, surname, age)
+fmt.Printf("Имя: %s, Фамилия: %s, Возраст: %d, Студент BPS", name2, surname2, age2)
+
 }
